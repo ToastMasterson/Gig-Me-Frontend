cdf3b7339e2691d0abff2ce813a97865de522309
github username <github email address>
2019-10-15 16:50:45 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style_sheets/App.css';
+import LogIn from './components/LogIn';
+import NavBar from './NavBar/NavBar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ArtistHome from './containers/ArtistHome'
+import { useAuth0 } from './react-auth0-wrapper'
+import PrivateRoute from './components/PrivateRoute'
+import ExternalApi from './components/ExternalApi';
 
 function App() {
+  const { loading } = useAuth0()
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+          <p>Work In Progress...</p>
+          {/* <LogIn /> */}
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/artisthome" component={ArtistHome} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/LogIn.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style_sheets/LogIn.css'
+const LogIn = () => {
+    return(
+        <div className="main">
+            <h1>Gig-Me</h1>
+            <h2>Coming Soon</h2>
+            <div className="log-in">
+                <button className="log-in-button">
+                    Log-In
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default LogIn
